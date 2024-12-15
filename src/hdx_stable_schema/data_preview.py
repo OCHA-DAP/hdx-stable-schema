6655afbd0a4c5b91a9a87a3dc28182a083329819
Ian Hopkinson <ianhopkinson@googlemail.com>
2024-12-15 15:15:42 +0000

    HDXDSYS-1196 Fix platform dependent path issue

--- src/hdx_stable_schema/data_preview.py
+++ src/hdx_stable_schema/data_preview.py
@@ -3,7 +3,7 @@
 
 import ast
 import datetime
-import pathlib
+import glob
 import shutil
 import sys
 import zipfile
@@ -163,7 +163,8 @@ def load_dataframe_from_local_path(
         unzip_directory = Path(local_file_path).parent
         with zipfile.ZipFile(local_file_path, "r") as zip_file:
             zip_file.extractall(unzip_directory)
-        geo_files = sorted(pathlib.Path(unzip_directory).glob(f"**/*.{file_format}"))
+
+        geo_files = sorted(glob.glob(str(Path(unzip_directory) / "**" / f"*.{file_format}")))
         error_message = "Got more than one file of the right format from a zip"
         local_file_path = str(geo_files[0])
 
